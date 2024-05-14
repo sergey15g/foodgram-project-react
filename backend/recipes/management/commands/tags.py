229783c850bbfb6d825244e6b41e8c11b976f2bb
Сергей Гордон <sergey15g@gmail.com>
2024-05-14 09:48:20 +0300

    Add in tags 3 param

--- backend/recipes/management/commands/tags.py
+++ backend/recipes/management/commands/tags.py
@@ -9,6 +9,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         data = [
             {'name': 'Обед', 'color': '#49B64E', 'slug': 'dinner'},
-            {'name': 'Ужин', 'color': '#8775D2', 'slug': 'supper'}]
+            {'name': 'Ужин', 'color': '#8775D2', 'slug': 'supper'},
+            {'name': 'Завтрак', 'color': '#674EA7', 'slug': 'breakfast'}]
         Tag.objects.bulk_create(Tag(**tag) for tag in data)
         self.stdout.write(self.style.SUCCESS('Все тэги загружены!'))
